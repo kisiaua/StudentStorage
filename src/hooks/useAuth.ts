ccb34a1f79fd739076d7d0e1d8af7958dd7c735c
Konrad Kisiala <konradkisiala@gmail.com>
2024-06-13 00:33:54 +0200

    Add decoding JWT token to get user id and role

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,7 +2,50 @@ import { useContext } from "react";
 import AuthContext from "../context/AuthProvider.tsx";
 
 const useAuth = () => {
-  return useContext(AuthContext);
+  const { auth, setAuth } = useContext(AuthContext);
+
+  const decodeToken = (token: string) => {
+    try {
+      const base64Url = token.split(".")[1];
+      const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+      const jsonPayload = decodeURIComponent(
+        atob(base64)
+          .split("")
+          .map(function (c) {
+            return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+          })
+          .join("")
+      );
+      return JSON.parse(jsonPayload);
+    } catch (error) {
+      console.log("Invalid JWT Token", error);
+      return null;
+    }
+  };
+
+  const getUserId = () => {
+    if (!auth?.jwtAccessToken) {
+      return null;
+    }
+
+    const decodedToken = decodeToken(auth.jwtAccessToken);
+    return decodedToken?.[
+      "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"
+    ];
+  };
+
+  const getUserRole = () => {
+    if (!auth?.jwtAccessToken) {
+      return null;
+    }
+
+    const decodedToken = decodeToken(auth.jwtAccessToken);
+    return decodedToken?.[
+      "http://schemas.microsoft.com/ws/2008/06/identity/claims/role"
+    ];
+  };
+
+  return { auth, setAuth, getUserId, getUserRole };
 };
 
 export default useAuth;
